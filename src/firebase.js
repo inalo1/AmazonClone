4b513658ed14df43a76f3722e4f56f60ed7310fd
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2021-04-13 16:51:24 -0500

    Nav Bar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAc_YfnT1Ugly2kCduYMJIuGf9Dj3kgwUc",
+//     authDomain: "clone-be4d3.firebaseapp.com",
+//     projectId: "clone-be4d3",
+//     storageBucket: "clone-be4d3.appspot.com",
+//     messagingSenderId: "947421717223",
+//     appId: "1:947421717223:web:25cf0e21068edb35c3b842",
+//     measurementId: "G-1W2VPHPZT7"
+//   };
