1d7aafaacf0381f49c8b6bde346b94815b152bab
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2021-04-14 22:56:46 -0500

    creating an account, new user

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,32 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
 import './Login.css'
+import { Link, useHistory } from 'react-router-dom'
+import { auth } from './firebase'
 
 function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+    }
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // successfully created a new user with email/password
+                console.log(auth);
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -14,19 +37,27 @@ function Login() {
                 <h1>Sign In</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' />
+                    <input type='text' 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                    />
 
                     <h5>Password</h5>
-                    <input type='password' />
+                    <input type='password' 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
-                    <button className='login__signInButton'>Sign In</button>
+                    <button type='submit' onClick={signIn} 
+                        className='login__signInButton'>Sign In</button>
                 </form>
 
                 <p>
                     By continuing, you agree to the AMAZON CLONE APP's <a href='https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&nodeId=508088'>Conditions of Use </a> and <a href='https://www.amazon.com/gp/help/customer/display.html/ref=ap_signin_notification_privacy_notice?ie=UTF8&nodeId=468496'> Privacy Notice</a>.
                 </p>
 
-                <button className='login__registerButton'>Create your Amazon Account</button>
+                <button onClick={register}
+                    className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
     )
